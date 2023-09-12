db9c131c8c1139bccfafff8f5bdb48f4348f8ad2
Samet <samettaspinarx@gmail.com>
2023-09-12 16:05:40 -0400

    [UPD] training.py updated

---
+++ model_cnn/main.py
@@ -0,0 +1,49 @@
+import argparse
+import time
+
+import cv2
+import matplotlib.pyplot as plt
+import torch
+
+
+torch.manual_seed(1)
+
+ROOT_DIR = "/users/samet/Documents/data/TB_Chest_Radiography_Database"
+
+
+def main(root_dir=ROOT_DIR):
+    start_time = time.time()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--num_epochs", type=int, default=5, help="Number of training epochs"
+    )
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
+    parser.add_argument("--lr", type=float, default=5e-4, help="Learning rate")
+    parser.add_argument("--l2", type=float, default=0, help="L2 regularisation")
+    parser.add_argument(
+        "--aug",
+        action="store_true",
+        default=False,
+        help="Use data augmentation",
+    )
+    parser.add_argument(
+        "--data_path", type=str, default=root_dir, help="Path to data."
+    )
+    parser.add_argument(
+        "--bond_dim", type=int, default=5, help="MPS Bond dimension"
+    )
+    parser.add_argument(
+        "--nChannel", type=int, default=1, help="Number of input channels"
+    )
+    parser.add_argument(
+        "--dense_net",
+        action="store_true",
+        default=False,
+        help="Using Dense Net model",
+    )
+
+
+if __name__ == "__main__":
+    im = cv2.imread(f"{ROOT_DIR}/Normal/Normal-100.png")
+    plt.imshow(im)
+    main()
