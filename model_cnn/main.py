759a44c5aa5cc0eb41e3d58af1217397f6a79303
Samet <samettaspinarx@gmail.com>
2023-09-12 16:58:01 -0400

    [UPD] main.py fully functional now

--- model_cnn/main.py
+++ model_cnn/main.py
@@ -4,17 +4,19 @@ import time
 import cv2
 import matplotlib.pyplot as plt
 import torch
+from model import densenet121_model
+from dataset import get_loaders, ChestXRayDataset, get_transformation
 
+from constants import ROOT_DIR
+from training import train_model
 
 torch.manual_seed(1)
 
-ROOT_DIR = "/users/samet/Documents/data/TB_Chest_Radiography_Database"
 
-
-def parse_args(root_dir=ROOT_DIR):
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--num_epochs", type=int, default=5, help="Number of training epochs"
+        "--num_epochs", type=int, default=15, help="Number of training epochs"
     )
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
     parser.add_argument("--lr", type=float, default=5e-4, help="Learning rate")
@@ -26,7 +28,7 @@ def parse_args(root_dir=ROOT_DIR):
         help="Use data augmentation",
     )
     parser.add_argument(
-        "--data_path", type=str, default=root_dir, help="Path to data."
+        "--data_path", type=str, default=ROOT_DIR, help="Path to data."
     )
     parser.add_argument(
         "--bond_dim", type=int, default=5, help="MPS Bond dimension"
@@ -40,11 +42,31 @@ def parse_args(root_dir=ROOT_DIR):
         default=False,
         help="Using Dense Net model",
     )
+    user_args = parser.parse_args([])
+    return user_args
 
 
 def main():
     print(f"TRAINING START TIME: {time.time()}")
-    args = parse_args()
+    user_args = parse_args()
+
+    model, criterion, optimizer, scheduler = densenet121_model(user_args)
+
+    train_transform, valid_transform = get_transformation()
+    dataset = ChestXRayDataset(root_dir=ROOT_DIR, transform=train_transform)
+
+    train_loader, test_loader, val_loader = get_loaders(
+        dataset, split_ratios=[0.7, 0.15, 0.15], batch_size=user_args.batch_size
+    )
+    densenet_121 = train_model(
+        model,
+        criterion,
+        optimizer,
+        scheduler,
+        train_loader,
+        val_loader,
+        user_args=user_args,
+    )
 
 
 if __name__ == "__main__":
