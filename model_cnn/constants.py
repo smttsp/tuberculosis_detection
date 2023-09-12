7ccfb089cd138c80b5c61a0171fb6e477df458fe
Samet <samettaspinarx@gmail.com>
2023-09-12 16:56:52 -0400

    [UPD] constants created

---
+++ model_cnn/constants.py
@@ -0,0 +1,6 @@
+import torch
+import os
+
+ROOT_DIR = "/users/samet/Documents/data/TB_Chest_Radiography_Database"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
