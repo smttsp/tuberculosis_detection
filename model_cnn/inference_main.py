0600dd69a3cb0f832283dc845fd86501184204dc
Samet <samettaspinarx@gmail.com>
2023-09-13 17:25:38 -0400

    [UPD] inference_main.py file created. This will be entrypoint func for inference

---
+++ model_cnn/inference_main.py
@@ -0,0 +1,21 @@
+from evaluation import do_inference
+import torchvision
+import torch
+import torch.nn as nn
+from constants import CLASS_NAMES
+
+if __name__ == "__main__":
+    model = torchvision.models.densenet121(weights=None)
+    num_features = model.classifier.in_features
+    model.classifier = nn.Sequential(nn.Linear(num_features, 2), nn.Sigmoid())
+
+    save_path = "saved_models/densenet121/best_model.pth"
+    weights = torch.load(f=save_path)
+    model.load_state_dict(weights)
+
+    image_path = "/users/samet/Documents/data/TB_Chest_Radiography_Database/Normal/Normal-1821.png"
+    x = do_inference(model, image_path=image_path)
+    _, y_pred = torch.max(x, 1)
+    pos = y_pred.cpu().numpy()
+    print(f"Prediction is: {CLASS_NAMES[pos]}")
+    print("finito")
