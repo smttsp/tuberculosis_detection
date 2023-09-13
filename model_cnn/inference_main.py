27e88836d3d51e6605d5aacc0298391c0dbc63c5
Samet <samettaspinarx@gmail.com>
2023-09-13 19:11:59 -0400

    [UPD] inference_main.py updated

--- model_cnn/inference_main.py
+++ model_cnn/inference_main.py
@@ -5,7 +5,10 @@ from constants import CLASS_NAMES
 from evaluation import do_inference
 
 
-if __name__ == "__main__":
+import sys
+
+
+def predict(image_path):
     model = torchvision.models.densenet121(weights=None)
     num_features = model.classifier.in_features
     model.classifier = nn.Sequential(nn.Linear(num_features, 2), nn.Sigmoid())
@@ -14,8 +17,14 @@ if __name__ == "__main__":
     weights = torch.load(f=save_path)
     model.load_state_dict(weights)
 
-    image_path = "/Users/samet/Documents/data/TB_Chest_Radiography_Database/Normal/Normal-1821.png"
     x = do_inference(model, image_path=image_path)
     _, y_pred = torch.max(x, 1)
     pos = y_pred.cpu().numpy()
     print(f"Prediction is: {CLASS_NAMES[pos]}")
+
+
+if __name__ == "__main__":
+    image_path = sys.argv[1]
+    print(image_path)
+    # image_path = "/Users/samet/Documents/data/TB_Chest_Radiography_Database/Normal/Normal-1821.png"
+    predict(image_path)
