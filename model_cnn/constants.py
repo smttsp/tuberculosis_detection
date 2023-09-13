fe9df357ec13a00a49850421c06c23caf832f0ac
Samet <samettaspinarx@gmail.com>
2023-09-13 10:55:31 -0400

    [UPD] `DatasetType` added in constants

--- model_cnn/constants.py
+++ model_cnn/constants.py
@@ -1,12 +1,16 @@
 import os
 from datetime import datetime
+from enum import Enum
 
 import torch
 
 
 ROOT_DIR = "/users/samet/Documents/data/TB_Chest_Radiography_Database"
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+CLASS_NAMES = ("Normal", "Tuberculosis")
 
 
 def get_runtime_str():
@@ -24,3 +28,9 @@ def get_runtime_str():
 
 
 RUNTIME_STR = get_runtime_str()
+
+
+class DatasetType(str, Enum):
+    train = "train"
+    val = "val"
+    test = "test"
