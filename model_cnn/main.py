c57457814645c811aa768bff9c06405c4bb1d080
Samet <samettaspinarx@gmail.com>
2023-09-13 11:18:58 -0400

    [CLN] removed `parse_args` func which was no longer used

--- model_cnn/main.py
+++ model_cnn/main.py
@@ -9,40 +9,7 @@ from model import densenet121_model
 from training import train_model
 
 
-torch.manual_seed(1)
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--num_epochs", type=int, default=15, help="Number of training epochs"
-    )
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
-    parser.add_argument("--lr", type=float, default=5e-4, help="Learning rate")
-    parser.add_argument("--l2", type=float, default=0, help="L2 regularisation")
-    parser.add_argument(
-        "--aug",
-        action="store_true",
-        default=False,
-        help="Use data augmentation",
-    )
-    parser.add_argument(
-        "--data_path", type=str, default=ROOT_DIR, help="Path to data."
-    )
-    parser.add_argument(
-        "--bond_dim", type=int, default=5, help="MPS Bond dimension"
-    )
-    parser.add_argument(
-        "--nChannel", type=int, default=1, help="Number of input channels"
-    )
-    parser.add_argument(
-        "--dense_net",
-        action="store_true",
-        default=False,
-        help="Using Dense Net model",
-    )
-    user_args = parser.parse_args([])
-    return user_args
+torch.manual_seed(0)
 
 
 @hydra.main(
