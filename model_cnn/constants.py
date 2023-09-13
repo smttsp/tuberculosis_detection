fa2144179354aba88793aa1ffca49074cc38d1df
Samet <samettaspinarx@gmail.com>
2023-09-12 22:43:49 -0400

    [UPD] added runtime string

--- model_cnn/constants.py
+++ model_cnn/constants.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import torch
 
@@ -6,3 +7,20 @@ import torch
 ROOT_DIR = "/users/samet/Documents/data/TB_Chest_Radiography_Database"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_runtime_str():
+    """Getting datetime as a string"""
+
+    runtime_str = (
+        datetime.now()
+        .isoformat()
+        .replace(":", "")
+        .replace("-", "")
+        .replace("T", "-")
+        .split(".")[0]
+    )
+    return runtime_str
+
+
+RUNTIME_STR = get_runtime_str()
